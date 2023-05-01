01753253277de63e499abc56550ae5a24c2e0f0e
ganeshpadole <ganeshpadole55@gmail.com>
2023-05-01 23:34:28 +0530

    - code refactor

--- Filmfest_UnitTesting/Models/MovieManager.swift
+++ Filmfest_UnitTesting/Models/MovieManager.swift
@@ -8,14 +8,18 @@
 import Foundation
 
 class MovieManager {
-    var moviesToSeeCount = 0
-    var moviesSeenCount = 0
+    var moviesToSeeCount: Int {
+        return moviesToSeeArray.count
+    }
+    
+    var moviesSeenCount: Int {
+        return moviesSeenArray.count
+    }
     
     private var moviesToSeeArray = [Movie]()
     private var moviesSeenArray = [Movie]()
     
     func addMovie(movie: Movie) {
-        moviesToSeeCount += 1
         moviesToSeeArray.append(movie)
     }
     
@@ -26,8 +30,6 @@ class MovieManager {
     func checkOffMovieAtIndex(index: Int) {
         
         guard index < moviesToSeeCount else { return }
-        moviesToSeeCount -= 1
-        moviesSeenCount += 1
         
         let movie = moviesToSeeArray.remove(at: index)
         moviesSeenArray.append(movie)
